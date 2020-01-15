db130eceedca4911ead557336f84062b78bfdc43
Magurean Andrei <58299105+Magurean@users.noreply.github.com>
2020-01-16 00:11:51 +0200

    Add files via upload

---
+++ AutomatDeVanzari/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AutomatDeVanzari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int credit = 0;
+            int rest = 0;
+            Console.WriteLine("Cafea 1leu");
+            while (credit < 100)
+            {
+                Console.Write("Introduceti o moneda:");
+                int n = int.Parse(Console.ReadLine());
+                credit += n;
+                Console.WriteLine($"Ati introdus:{credit} bani");
+            }
+            if (credit >= 100)
+            {
+                Console.WriteLine("Cafeaua se pregateste");
+                rest = credit - 100;
+                Console.WriteLine($"Rest:{rest}");
+            }
+           
+
+
+        }
+    }
+}
